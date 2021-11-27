b72abbd9cf7988733a3a98694413b611c5f23e17
Max Sashcheka <sashchekam@gmail.com>
2021-11-27 17:14:40 +0300

    last commit

--- script.js
+++ script.js
@@ -1,89 +1,60 @@
-var passiveTableNameList = [];
 var passiveTableAmountList = [];
-
-var activeTableNameList = [];
 var activeTableAmountList = [];
-
-var passiveTableRowsNumber = 1;
-var passiveTableSelectedIndex = 0;
-
-var activeTableRowsNumber = 1;var activeTableSelectedIndex = 0;
 
 function AddRow(){
 
+	var name = document.getElementById("name").value;
+	var amount =  document.getElementById("amount").value;
+
+	if (name == "" || amount == "") {
+		alert("Вы не заполнили все поля. Пожалуйста, попробуйте еще раз.")
+		return
+	}
+
 	if (document.querySelector('input[name="passiveActive"]:checked').value == "Пассивный") {
 
-		passiveTableNameList[passiveTableSelectedIndex] = document.getElementById("name").value;
-		passiveTableAmountList[passiveTableSelectedIndex] = document.getElementById("amount").value;
+		var addRown = document.getElementById('passiveTable');
+		var newRow = addRown.insertRow(passiveTableAmountList.length + 1);
 
-		if (passiveTableNameList[passiveTableSelectedIndex] == "" || passiveTableAmountList[passiveTableSelectedIndex] == "") {
-			alert("Вы не заполнили все поля, пожалуйста, попробуйте еще раз.")
-			passiveTableNameList.pop
-			passiveTableAmountList.pop
-			return
-		}
-
-		var AddRown = document.getElementById('passiveTable');
-		var NewRow = AddRown.insertRow(passiveTableRowsNumber);
-
-		var cel1 = NewRow.insertCell(0);
-		var cel2 = NewRow.insertCell(1);
-		var cel3 = NewRow.insertCell(2);
+		var cel1 = newRow.insertCell(0);
+		var cel2 = newRow.insertCell(1);
+		var cel3 = newRow.insertCell(2);
 
 		cel1.innerHTML = document.querySelector('input[name="passiveActive"]:checked').value;
-		cel2.innerHTML = passiveTableNameList[passiveTableSelectedIndex];
-		cel3.innerHTML = passiveTableAmountList[passiveTableSelectedIndex];
+		cel2.innerHTML = name;
+		cel3.innerHTML = amount;
 
-		passiveTableRowsNumber++;
-		passiveTableSelectedIndex++;
+		passiveTableAmountList.push(amount)
 
 	} else {
 
-		activeTableNameList[activeTableSelectedIndex] = document.getElementById("name").value;
-		activeTableAmountList[activeTableSelectedIndex] = document.getElementById("amount").value;
+		var addRown = document.getElementById('activeTable');
+		var newRow = addRown.insertRow(activeTableAmountList.length + 1);
 
-		if (activeTableNameList[activeTableSelectedIndex] == "" || activeTableAmountList[activeTableSelectedIndex] == "") {
-			alert("Вы не заполнили все поля, пожалуйста, попробуйте еще раз.")
-			activeTableNameList.pop
-			activeTableAmountList.pop
-			return
-		}
-
-		var AddRown = document.getElementById('activeTable');
-		var NewRow = AddRown.insertRow(activeTableRowsNumber);
-
-		var cel1 = NewRow.insertCell(0);
-		var cel2 = NewRow.insertCell(1);
-		var cel3 = NewRow.insertCell(2);
+		var cel1 = newRow.insertCell(0);
+		var cel2 = newRow.insertCell(1);
+		var cel3 = newRow.insertCell(2);
 
 		cel1.innerHTML = document.querySelector('input[name="passiveActive"]:checked').value;
-		cel2.innerHTML = activeTableNameList[activeTableSelectedIndex];
-		cel3.innerHTML = activeTableAmountList[activeTableSelectedIndex];
+		cel2.innerHTML = name;
+		cel3.innerHTML = amount;
 
-		activeTableRowsNumber++;
-		activeTableSelectedIndex++;
+		activeTableAmountList.push(amount)
 
 	}
-
 			
 }
 
 function calculateActive() {
 	var sum = 0;
-	activeTableAmountList.forEach(element => {
-		sum += parseInt(element)
-	});
-
+	activeTableAmountList.forEach(element => { sum += parseInt(element) });
     document.getElementById("currentActiveSummary").innerHTML = "Текущая сумма активов: " + sum;   
 	return sum
 }
 
 function calculatePassive() {
 	var sum = 0;
-	passiveTableAmountList.forEach(element => {
-		sum += parseInt(element)
-	});
-
+	passiveTableAmountList.forEach(element => { sum += parseInt(element) });
     document.getElementById("currentPassiveSummary").innerHTML = "Текущая сумма пасивов: " + sum; 
 	return sum
 }
@@ -91,9 +62,7 @@ function calculatePassive() {
 function calculateTotal() {
 	var activeSum = calculateActive();
 	var passiveSum = calculatePassive();
-
 	var totalSum = activeSum - passiveSum;
 
     document.getElementById("currentTotalSummary").innerHTML = "Текущий итоговый баланс: " + totalSum; 
-
 }
